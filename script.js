123b4283069e3a286953a32717e149500e9e10e2
Haifa <ffathimahaifa@gmail.com>
2026-05-20 23:22:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+  function validform() { <!-- fun braket open -->
+  let name=document.getElementById("name").value;
+  let email=document.getElementById("email").value;
+        
+        if (name=="" || email=="") {
+          alert ("all fields must be filled ");
+          return false;
+        }
+        alert ("success");
+        return true;
+        
+     } <!-- fun braket close -->
+
